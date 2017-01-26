8bd46cec933befd5bdec884bc177e492b7795a4e
Gamerlazer <gamerlazer@gmail.com>
2017-01-25 18:29:04 -0800

    Update env port for deployment and npm start forever

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+if (process.env.PROD.PORT) {
+  const port = process.env.PROD.PORT
+} else {
+  const port = 3000
+}
 
 app.use('/', express.static(`${__dirname}/../client`))
 
